20bbedbe38b112667d220b15c017db7ca56300ff
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2024-01-09 20:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 //your JS code here. If required.
-function name(str) {
+function mapLetters(str) {
 	let ans={};
 	for(let i=0;i<str.length;i++){
-		if(ans[str[i]]===true){
+		if(ans[str[i]]){
 			ans[str[i]].push(i);
 		}
 		else{
 			ans[str[i]]=[i];
-			// ans["name"]=["sanu"];
-			// ans.name="sanu"
 		}
 	}
 	return ans;
 }
 let str=prompt();
-alert(name(str));
+let obj=mapLetters(str);
+console.log(obj);
