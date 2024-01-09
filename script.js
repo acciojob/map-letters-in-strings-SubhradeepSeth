a6b08c07d2b608b3f278f1a3668382273afd97d1
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2024-01-09 11:39:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function name(str) {
+	let ans={};
+	for(let i=0;i<str.length;i++){
+		if(ans[str[i]]===true){
+			ans[str[i]].push(i);
+		}
+		else{
+			ans[str[i]]=[i];
+			// ans["name"]=["sanu"];
+			// ans.name="sanu"
+		}
+	}
+	return ans;
+}
+let str=prompt();
+alert(name(str));
